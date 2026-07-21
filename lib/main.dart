150806a17b22e7eeb886b387bd1a55a205a4b7de
Guilherme Azeredo <guilhermesilvaazeredo12@gmail.com>
2026-07-20 21:43:44 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Bom Dia!',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 30.0),
+      ),
+    ),
+  );
+}
