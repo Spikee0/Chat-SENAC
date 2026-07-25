931cc31850b92fb7969cde4f8d13270526e24641
Guilherme Azeredo <guilhermesilvaazeredo12@gmail.com>
2026-07-24 21:40:53 -0300

    terceiro commit(incompleto)

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
- @override
-  Widget build(BuildContext context) {
-   return MaterialApp(
-     home: Scaffold(
-       body: Login(
 
-       ),
-     ),
-   );
- }
-}
-class Login extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Login()));
+  }
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
-        FlutterLogo(size: 18,),
-        Text('ChatSENAC'),
+        Row(children: [FlutterLogo(size: 18), Text('ChatSENAC')]),
+        SizedBox(height: 32),
+
         Text("Entre Na sua conta"),
         Text("Coloque o seu email e senha para logar"),
         Text("email"),
@@ -32,7 +29,10 @@ class Login extends StatelessWidget{
         ElevatedButton(onPressed: null, child: Text("Entrar")),
         Text("Ou"),
         ElevatedButton(onPressed: null, child: Text("Continuar com o google")),
-        ElevatedButton(onPressed: null, child: Text("Continuar com o faceBook?")),
+        ElevatedButton(
+          onPressed: null,
+          child: Text("Continuar com o faceBook?"),
+        ),
         Text("não tem uma conta?"),
         InkWell(child: Text("Cadastre-se")),
       ],
