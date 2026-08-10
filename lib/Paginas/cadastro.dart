2e7b8acd2a594faa9717e4ed20ece7853b3da767
Guilherme Azeredo <guilhermesilvaazeredo12@gmail.com>
2026-08-10 19:52:05 -0300

    Foi realizada a validação da tela de cadastros, responsavel por inserir usuarios.

--- lib/Paginas/cadastro.dart
+++ lib/Paginas/cadastro.dart
@@ -21,146 +21,159 @@ class _CadastroState extends State<Cadastro> {
   final emailController = TextEditingController();
   final senhaController = TextEditingController();
   final nomeController = TextEditingController();
+  final ConfSenhaController = TextEditingController();
 
-  Future<void> fazerCadastro() async{
-         var url = Uri.http("10.112.4.33", "/api/cadastro");
-         var resposta = await http.post(
-           url,
-           headers: {
-             "content-type": "application/json"
-           },
-           body: jsonEncode(
-               {'email': emailController.text, 'senha': senhaController.text, 'nome': nomeController.text}),
-         );
-         if (resposta.statusCode != 200) {
-           var dados = jsonDecode(resposta.body);
+  Future<void> fazerCadastro() async {
+    if (ConfSenhaController.text.isEmpty) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Confirme sua senha")));
+      return;
+    }
 
-           ScaffoldMessenger.of(context).showSnackBar(
-             SnackBar(content: Text("${dados["message"]}")),
-           );
-           return;
-         }
-         else if(senhaController != ){
+    if (ConfSenhaController.text != senhaController.text) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("As senhas não coincidem")));
+      return;
+    }
 
-         }
-         Navigator.push(
-             context, MaterialPageRoute(builder: (build) => Login()));
-        }
+    var url = Uri.http("10.112.4.33", "/api/cadastro");
+    var resposta = await http.post(
+      url,
+      headers: {"content-type": "application/json"},
+      body: jsonEncode({
+        'email': emailController.text,
+        'senha': senhaController.text,
+        'nome': nomeController.text,
+      }),
+    );
+    if (resposta.statusCode != 201) {
+      var dados = jsonDecode(resposta.body);
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsetsGeometry.symmetric(
-                horizontal: 24.0,
-                vertical: 16.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: GestureDetector(
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("${dados["message"]}")));
+      return;
+    }
+    Navigator.push(context, MaterialPageRoute(builder: (build) => Login()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsetsGeometry.symmetric(
+              horizontal: 24.0,
+              vertical: 16.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(
+                        context,
+                        MaterialPageRoute(builder: (context) => Login()),
+                      );
+                    },
+                    child: Icon(Icons.arrow_back),
+                  ),
+                ),
+                SizedBox(height: 36),
+                Text("Cadastre-se", style: Tipografia.h1),
+                SizedBox(height: 18),
+                Text(
+                  "Crie uma conta para continuar!",
+                  style: Tipografia.subtitulo,
+                ),
+                SizedBox(height: 36),
+                Text("Nome", style: Tipografia.subtitulo),
+                TextField(
+                  controller: nomeController,
+                  decoration: InputDecoration(
+                    hint: Text("Usuario@gmail.com"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 18),
+                Text("Email", style: Tipografia.subtitulo),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hint: Text("Usuario@gmail.com"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 18),
+                Text("Senha", style: Tipografia.subtitulo),
+                TextField(
+                  controller: senhaController,
+                  decoration: InputDecoration(
+                    hint: Text("*******"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 18),
+                Text("Confirmar Senha", style: Tipografia.subtitulo),
+                TextField(
+                  controller: ConfSenhaController,
+                  decoration: InputDecoration(
+                    hint: Text("*******"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: fazerCadastro,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(50, 110, 250, 10),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.all(18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(10),
+                    ),
+                  ),
+                  child: Text("Cadastrar"),
+                ),
+                SizedBox(height: 36),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Já tem uma conta?"),
+                    InkWell(
                       onTap: () {
                         Navigator.pop(
                           context,
                           MaterialPageRoute(builder: (context) => Login()),
                         );
                       },
-                      child: Icon(Icons.arrow_back),
+                      child: Text("  Login", style: Tipografia.link),
                     ),
-                  ),
-                  SizedBox(height: 36),
-                  Text("Cadastre-se", style: Tipografia.h1),
-                  SizedBox(height: 18),
-                  Text(
-                    "Crie uma conta para continuar!",
-                    style: Tipografia.subtitulo,
-                  ),
-                  SizedBox(height: 36),
-                  Text("Nome", style: Tipografia.subtitulo),
-                  TextField(
-                    controller: nomeController,
-                    decoration: InputDecoration(
-                      hint: Text("Usuario@gmail.com"),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 18),
-                  Text("Email", style: Tipografia.subtitulo),
-                  TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      hint: Text("Usuario@gmail.com"),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 18),
-                  Text("Senha", style: Tipografia.subtitulo),
-                  TextField(
-                    controller: senhaController,
-                    decoration: InputDecoration(
-                      hint: Text("*******"),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 18),
-                  Text("Confirmar Senha", style: Tipografia.subtitulo),
-                  TextField(
-                    decoration: InputDecoration(
-                      hint: Text("*******"),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 24),
-                  ElevatedButton(
-                    onPressed: fazerCadastro,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(50, 110, 250, 10),
-                      foregroundColor: Colors.white,
-                      padding: EdgeInsets.all(18),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadiusGeometry.circular(10),
-                      ),
-                    ),
-                    child: Text("Entrar"),
-                  ),
-                  SizedBox(height: 36),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Já tem uma conta?"),
-                      InkWell(
-                        onTap: () {
-                          Navigator.pop(
-                            context,
-                            MaterialPageRoute(builder: (context) => Login()),
-                          );
-                        },
-                        child: Text("  Login", style: Tipografia.link),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
-      );
-    }
+      ),
+    );
   }
+}
